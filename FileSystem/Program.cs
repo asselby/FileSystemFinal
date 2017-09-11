678cbd9522806886a61350b87e9e9dcfe301aefc
Besttdk <Besttdk@ASSELBY>
2017-09-11 14:29:26 +0600

    Добавьте файлы проекта.

---
+++ FileSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿/*1.	В файле записана непустая последовательность целых чисел, являющихся числами Фибоначчи.
+ *  Приписать еще столько же чисел этой последовательности.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\root\filesystem.txt";
+            string fibonacciNumber = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,";
+            string fibonacciNumberSecond =  "233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946" ;
+            FileInfo file = new FileInfo(path);
+            using (StreamWriter writer = File.CreateText(path))
+            {
+               
+                writer.WriteLine(fibonacciNumber);
+            }
+
+            using (StreamWriter secondWriter = File.AppendText(path))
+            {
+                secondWriter.WriteLine(fibonacciNumberSecond);
+            }
+            using (StreamReader reader = File.OpenText(path))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.Write(line);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SumOfTwoElements/Program.cs
@@ -0,0 +1,45 @@
+﻿/*2.	Сложить два целых числа А и В.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SumOfTwoElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\root\Input.txt";
+            FileInfo file = new FileInfo(path);
+
+            //int A = 12;
+            //int B = 13;
+            int sumOfAB;
+            //using (StreamWriter writer = File.CreateText(path))
+            //{
+            //    writer.Write(A);
+            //    writer.Write(" ");
+            //    writer.Write(B);
+            //}
+                 
+            // чтение из файла
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                int textFromFile = System.Text.Encoding.Default.
+                Console.WriteLine("Текст из файла: {0}", textFromFile);
+          
+            }
+
+            Console.ReadLine();
+   
+        }
+    }
+}
