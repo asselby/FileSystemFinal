edab1f3abd04adc02a8ac915a7f878db098b9e4c
Besttdk <Besttdk@ASSELBY>
2017-09-11 22:34:07 +0600

    in SumOfTwoElements was changed

--- SumOfTwoElements/Program.cs
+++ SumOfTwoElements/Program.cs
@@ -13,33 +13,47 @@ namespace SumOfTwoElements
         static void Main(string[] args)
         {
             string path = @"C:\root\Input.txt";
+            string pathSecond = @"C:\root\Output.txt";
             FileInfo file = new FileInfo(path);
-
-            //int A = 12;
-            //int B = 13;
+            int A = 12;
+            int B = 13;
             int sumOfAB;
-            //using (StreamWriter writer = File.CreateText(path))
-            //{
-            //    writer.Write(A);
-            //    writer.Write(" ");
-            //    writer.Write(B);
-            //}
-                 
-            // чтение из файла
-            using (FileStream fstream = File.OpenRead(path))
+            int numberA;
+            int numberB;
+            try
             {
-                // преобразуем строку в байты
-                byte[] array = new byte[fstream.Length];
-                // считываем данные
-                fstream.Read(array, 0, array.Length);
-                // декодируем байты в строку
-                int textFromFile = System.Text.Encoding.Default.
-                Console.WriteLine("Текст из файла: {0}", textFromFile);
-          
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                {
+                    writer.Write(A);
+                    writer.Write(B);
+                }
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    while (reader.PeekChar() > -1)
+                    {
+                        numberA = reader.ReadInt32();
+                        reader.PeekChar();
+                        numberB = reader.ReadInt32();
+                        sumOfAB = numberA + numberB;
+                        Console.WriteLine("Певрое число - {0}, второе число - {1}. ", numberA, numberB);
+                        using (BinaryWriter writerSum = new BinaryWriter(File.Open(pathSecond, FileMode.OpenOrCreate)))
+                        {
+                            writerSum.Write(sumOfAB);
+                        }
+                        using (BinaryReader readerSum = new BinaryReader(File.Open(pathSecond, FileMode.Open)))
+                        {
+                            Console.WriteLine("Сумма двух чисел - {0}.", readerSum.Read());
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
             Console.ReadLine();
-   
         }
     }
 }
